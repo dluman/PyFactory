8a74a9e9192c19fc67001a85d76c2c71424fdeba
dluman <dluman@allegheny.edu>
2024-02-07 16:22:50 -0600

    Updating with for development install

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ import setuptools
 setuptools.setup(
     name="springform",
     version="0.6.5",
-    packages=['springform'],
-    package_dir={'springform': 'src'},
+    packages=["springform"],
     include_package_data=True,
     description="A simple templating system for Python class files.",
     long_description="""
@@ -28,4 +27,4 @@ other modules on PATH. As of 0.2.0, inheritances are possible.
       '',
     ],
     url="https://github.com/dluman/springform"
- )
+)
