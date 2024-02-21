3b0ce31d22fc482d217b7db840f06b0e2f18dc06
dluman <dluman@allegheny.edu>
2024-02-21 16:06:21 -0500

    Removing ridiculous old folder

--- old/springform/Errors.py
+++
@@ -1,5 +0,0 @@
-class BadModuleFormatException(Exception):
-
-  def __init__(self, filename: str = "", *args):
-    super().__init__(args)
-    print("Incorrect module format: not a Python file or Pickle.")
