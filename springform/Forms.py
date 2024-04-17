2a807a1c06ce77df1ad6d21f853bb4fa0a86416a
Daniel Bekele <danielbekele@Daniels-MacBook-Pro-3.local>
2024-04-17 15:18:35 -0400

    qualname

--- springform/Forms.py
+++ springform/Forms.py
@@ -34,16 +34,36 @@ class Form:
     except:
       raise BadModuleFormatException
 
+
   def __mainify(self):
     if self.instance.__module__ != "__main__":
-      # Below only grabs the original code
-      #source = inspect.getsource(self.instance)
-      setattr(self.instance,"__module__","")
-      source = inspect.getsource(ctypes.cast(id(self.instance), ctypes.py_object).value)
-      co = compile(source, '<string>', 'exec')
-      exec(co, __main__.__dict__)
-      for prop in self.instance.__dict__:
-        bmp.type_set(__main__.__dict__[self.instance.__name__], prop, self.instance.__dict__[prop])
+        # Temporarily change the __module__ attribute to '__main__'
+        # Get a pointer to the instance object
+        ptr = ctypes.cast(id(self.instance), ctypes.py_object)
+        
+        # Change the __module__ to '__main__'
+        ptr.value.__module__ = "__main__"
+        
+        # Optionally, copy the class definition to __main__ if not already there
+        class_name = self.instance.__class__.__name__
+        if class_name not in __main__.__dict__:
+            # Create a shallow copy of the class with a new name in __main__
+            new_class = type(class_name, self.instance.__class__.__bases__, dict(self.instance.__class__.__dict__))
+            __main__.__dict__[class_name] = new_class
+        
+        # Reassign the instance's class to the new class in __main__
+        self.instance.__class__ = __main__.__dict__[class_name]
+
+  # def __mainify(self):
+  #   if self.instance.__module__ != "__main__":
+  #     # Below only grabs the original code
+  #     #source = inspect.getsource(self.instance)
+  #     setattr(self.instance,"__module__","")
+  #     source = inspect.getsource(ctypes.cast(id(self.instance), ctypes.py_object).value)
+  #     co = compile(source, '<string>', 'exec')
+  #     exec(co, __main__.__dict__)
+  #     for prop in self.instance.__dict__:
+  #       bmp.type_set(__main__.__dict__[self.instance.__name__], prop, self.instance.__dict__[prop])
 
   def make_dillable(self, path: str = "") -> str:
     self.__mainify()
@@ -52,6 +72,7 @@ class Form:
     with open(f"{path}{self.instance.__name__}", "wb") as fh:
       dill.dump(cls, fh)
     return self.instance.__name__
+
 
   def add_props(self, **kwargs) -> None:
     for arg in kwargs:
